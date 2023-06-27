e7b23d891bcdaafc320dd4faa7d451b9f60bb44a
Dan Buchholz <dbuchholz30@gmail.com>
2023-06-26 17:43:18 -0700

    fix: use hardhat chain over localhost for LT
    The 'chain.hardhat' must be used instead of 'chain.localhost' due to chain ID 31337 Local Tableland requirement.
    
    Signed-off-by: Dan Buchholz <dbuchholz30@gmail.com>

--- src/app/components/ContextProviders.tsx
+++ src/app/components/ContextProviders.tsx
@@ -1,45 +1,49 @@
-import * as React from 'react';
-import { FlagsProvider } from 'react-feature-flags';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import store from '../store/store';
-import GrandCentral from './GrandCentral';
+import * as React from "react";
+import { FlagsProvider } from "react-feature-flags";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "../store/store";
+import GrandCentral from "./GrandCentral";
 
 import {
   getDefaultWallets,
   RainbowKitProvider,
   darkTheme,
-  Chain
-} from '@rainbow-me/rainbowkit';
-import {
-  configureChains,
-    createConfig,
-  WagmiConfig,
-} from 'wagmi';
-import { publicProvider } from 'wagmi/providers/public';
-import * as chain from 'wagmi/chains';
+  Chain,
+} from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import * as chain from "wagmi/chains";
 
 const { chains, publicClient } = configureChains(
-  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum, chain.arbitrumGoerli, chain.sepolia, chain.polygonMumbai, chain.optimismGoerli, chain.filecoinCalibration, chain.localhost],
   [
-    publicProvider(),
-  ]
+    chain.mainnet,
+    chain.polygon,
+    chain.optimism,
+    chain.arbitrum,
+    chain.arbitrumGoerli,
+    chain.sepolia,
+    chain.polygonMumbai,
+    chain.optimismGoerli,
+    chain.filecoinCalibration,
+    chain.hardhat,
+  ],
+  [publicProvider()]
 );
 
 const { connectors } = getDefaultWallets({
-  appName: 'Tableland Dashboard',
+  appName: "Tableland Dashboard",
   chains,
-  projectId:process.env.REACT_APP_WALLET_CONNECT_API_KEY ?? "",
+  projectId: process.env.REACT_APP_WALLET_CONNECT_API_KEY ?? "",
 });
 
 const wagmiClient = createConfig({
   autoConnect: true,
   connectors,
-  publicClient
+  publicClient,
 });
 
-function Main() { 
-  
+function Main() {
   return (
     <Provider store={store}>
       <FlagsProvider value={store.getState().flags}>
